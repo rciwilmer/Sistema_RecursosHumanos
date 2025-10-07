c13bfc4c70766bf84ccdec6ddbd5cb0bb4d95c21
ANDERSON JAIR DURAND SALAZAR <1619139@senati.pe>
2025-10-07 10:07:35 -0500

    formulario de Anderson

--- Sistema_RecursosHumanos/anderson.Designer.cs
+++ Sistema_RecursosHumanos/anderson.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(142, 65);
+            this.button1.Location = new System.Drawing.Point(168, 90);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(428, 216);
             this.button1.TabIndex = 0;
